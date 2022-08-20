8f2f5817b460b219e7daf4eed26c37a20915461f
hafiz <hafizn24@gmail.com>
2022-08-20 09:08:58 +0800

    Project - FYP- Done

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,4 +1,4 @@
-import { Box, Grid, Typography, CardMedia } from "@mui/material"
+import { Box, Grid, Typography, CardMedia, Button } from "@mui/material"
 
 import fyp from './images/fyp_colage.png'
 
@@ -10,14 +10,14 @@ function Project() {
         justifyContent="flex-start"
         alignItems="center"
         pt={2}
-        sx={{border: '1px dashed grey'}}
+        // sx={{border: '1px dashed grey'}}
     >
         <Grid
             container
             justifyContent="center"
             alignItems="center"
         >
-            <Typography variant="h4">
+            <Typography variant="h4" sx={{m:2}}>
                 Projects
             </Typography>
         </Grid>
@@ -25,7 +25,7 @@ function Project() {
             ml:{
                 lg:3
             },
-            border: '1px dashed grey',
+            // border: '1px dashed grey',
             width: {
                 lg: "50%"
             }
@@ -39,20 +39,59 @@ function Project() {
         </Box>
         <Box sx={{
             m:1,
-            border: '1px dashed grey',
+            ml:{
+                lg:3
+            }
+            // border: '1px dashed grey',
         }}>
-            <Typography variant="h6">
+            <Typography variant="h5" color="primary" sx={{ fontWeight: 'bold' }}>
                 Final Year Project
+            </Typography>
+            <Grid
+                container
+                direction="row"
+            >
+                <Typography variant="body2">
+                    The system can detect a face mask by implementing a 
                 </Typography>
-            <Typography variant="subtitle1">
-                The system can detect a face mask by implementing a CNN classification algorithm.
-            </Typography>
-            <Typography variant="subtitle1">
-                This study focuses on two CNN models with different optimizers
-            </Typography>
-            <Typography variant="subtitle1">
-                trained with an image dataset labeled in two separate class labels.
-            </Typography>
+                <Typography variant="body2" color="primary" sx={{ fontWeight: 'bold', ml:0.5 }}>
+                    CNN classification algorithm
+                </Typography>
+                <Typography variant="body2">.</Typography>
+            </Grid>
+            
+            <Grid
+                container
+                direction="row"
+            >
+                <Typography variant="body2">
+                    This study focuses on two
+                </Typography>
+                <Typography variant="body2" color="primary" sx={{ fontWeight: 'bold', mx:0.5 }}>
+                    CNN models
+                </Typography>
+                <Typography variant="body2">
+                    with different 
+                </Typography>
+                <Typography variant="body2" color="primary" sx={{ fontWeight: 'bold', ml:0.5 }}>
+                    optimizers
+                </Typography>
+            </Grid>
+            <Grid
+            container
+            direction="row"
+            >
+                <Typography variant="body2">
+                    trained with an image dataset labeled in two separate
+                </Typography>
+                <Typography variant="body2" color="primary" sx={{ fontWeight: 'bold', ml:0.5 }}>
+                    class labels
+                </Typography>
+                <Typography variant="body2">
+                    .
+                </Typography>
+            </Grid>
+            <Button variant="outlined" sx={{mt:2, px:4}}>Learn More</Button>
         </Box>
     </Grid>
   )
