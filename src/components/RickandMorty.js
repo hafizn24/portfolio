4da38ed917feb1f0f2037d4578d4d3ba88d5ebaf
hafiz <hafizn24@gmail.com>
2022-09-05 21:23:29 +0800

    api fetching system

--- src/components/RickandMorty.js
+++ src/components/RickandMorty.js
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
 function RickandMorty() {
+  const[list, setList] = useState("")
+
+  
+  useEffect(() => {
+    const url = 'https://rickandmortyapi.com/api/character'
+    const fetchData = async() =>{
+      try{
+        var response = await fetch(url)
+        var tempJson = await response.json()
+
+        const tempList = []
+
+        while(tempJson.info.next != null){ 
+          for(let i in tempJson.results){
+            tempList.push(tempJson.results[i])
+          }
+
+          response = await fetch(tempJson.info.next)
+          tempJson = await response.json()
+        }
+        setList(tempList)
+      }
+      catch(error){
+        console.log(error)
+      }
+    }
+    fetchData()
+  },[])
+
   return (
-    <div>RickandMorty</div>
+    <div>
+      {list[0].name}
+    </div>
   )
 }
 
