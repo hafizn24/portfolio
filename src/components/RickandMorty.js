28c09459f8cbebdcb32d9241a10f6e6967b6cf87
hafiz <hafizn24@gmail.com>
2022-09-07 01:21:25 +0800

    able to fecth api; unable to display data

--- src/components/RickandMorty.js
+++ src/components/RickandMorty.js
@@ -1,28 +1,30 @@
 import React, {useEffect, useState} from 'react'
 
 function RickandMorty() {
-  const[arrayList, setArrayList] = useState()
 
+  const [arrayList, setArrayList] = useState('')
   useEffect(() => {
     const url = 'https://rickandmortyapi.com/api/character'
+
     const fetchData = async() =>{
       try{
-        let tempUrl = []
-        // tempUrl.push(url)
+        const tempInfo = []
+        let tempUrl = ''
 
         let response = await fetch(url)
         let tempJson = await response.json()
 
         while(tempJson.info.next != null){
+          tempUrl = tempJson.info.next
 
           for(let i in tempJson.results){
-            tempUrl.push(tempJson.results[i].name)
+            tempInfo.push(tempJson.results[i])
           }
 
           response = await fetch(tempJson.info.next)
           tempJson = await response.json()
         }
-        console.log(tempUrl)
+        setArrayList(tempInfo)
       }
       catch(error){
         console.log(error)
@@ -33,7 +35,7 @@ function RickandMorty() {
 
   return (
     <div>
-      Test
+      {console.log(arrayList[0])}
     </div>
   )
 }
