7cd26418a5e578d0ea021e0711662e64e6c09e68
hafizn24 <41638447+hafizn24@users.noreply.github.com>
2022-09-07 11:53:20 +0800

    Create RickandMortyMain.js
    add rickandmortymain page

---
+++ src/components/RickandMortyMain.js
@@ -0,0 +1,47 @@
+import React, {useEffect, useState} from 'react'
+
+import RickandMorty from './RickandMorty'
+
+function RickandMortyMain() {
+
+  const [arrayList, setArrayList] = useState('')
+  useEffect(() =>RickandMortyMain {
+    const url = 'https://rickandmortyapi.com/api/character'
+
+    const fetchData = async() =>{
+      try{
+        const tempInfo = []
+        let tempUrl = ''
+
+        let response = await fetch(url)
+        let tempJson = await response.json()
+
+        while(tempJson.info.next != null){
+          tempUrl = tempJson.info.next
+
+          for(let i in tempJson.results){
+            tempInfo.push(tempJson.results[i])
+          }
+
+          response = await fetch(tempJson.info.next)
+          tempJson = await response.json()
+        }
+        setArrayList(tempInfo)
+      }
+      catch(error){
+        console.log(error)
+      }
+    }
+    fetchData()
+  },[])
+
+  return (
+    <div>
+      <RickandMorty 
+        array={arrayList}
+      />
+    </div>
+  )
+}
+
+export default RickandMortyMain
