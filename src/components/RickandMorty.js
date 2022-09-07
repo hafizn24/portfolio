0daa529a9d453b3037b892180f5db424f1f432e9
hafizn24 <41638447+hafizn24@users.noreply.github.com>
2022-09-07 11:53:17 +0800

    Update RickandMorty.js
    add rickandmortymain page

--- src/components/RickandMorty.js
+++ src/components/RickandMorty.js
@@ -1,37 +1,6 @@
 import React, {useEffect, useState} from 'react'
 
-function RickandMorty() {
-
-  const [arrayList, setArrayList] = useState('')
-  useEffect(() => {
-    const url = 'https://rickandmortyapi.com/api/character'
-
-    const fetchData = async() =>{
-      try{
-        const tempInfo = []
-        let tempUrl = ''
-
-        let response = await fetch(url)
-        let tempJson = await response.json()
-
-        while(tempJson.info.next != null){
-          tempUrl = tempJson.info.next
-
-          for(let i in tempJson.results){
-            tempInfo.push(tempJson.results[i])
-          }
-
-          response = await fetch(tempJson.info.next)
-          tempJson = await response.json()
-        }
-        setArrayList(tempInfo)
-      }
-      catch(error){
-        console.log(error)
-      }
-    }
-    fetchData()
-  },[])
+function RickandMorty({array}) {
 
   return (
     <div>
