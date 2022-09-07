78e6dc80e290da8b7baff6fccf69d49e2c278c66
hafiz <hafizn24@gmail.com>
2022-09-07 20:44:15 +0800

    add simple loading

--- src/components/RickandMorty.js
+++ src/components/RickandMorty.js
@@ -1,4 +1,7 @@
 import React, {useEffect, useState} from 'react'
+import { CardMedia, Card } from '@mui/material'
+
+import loading from './images/loading.gif'
 
 function RickandMorty({array}) {
   var name = ''
@@ -10,6 +13,15 @@ function RickandMorty({array}) {
       name = array[array.length - 1].name
       status = array[array.length - 1].status
       species = array[array.length - 1].species
+    }else{
+      return(
+        <CardMedia 
+          component="img"
+          alt={loading}
+          height="300px"
+          image={loading}
+        />
+      )
     }
   }
 
