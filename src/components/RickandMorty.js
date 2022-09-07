33b0d8d4393b78ee443dc506b04a3b0a2d679def
hafizn24 <41638447+hafizn24@users.noreply.github.com>
2022-09-07 12:09:03 +0800

    Update RickandMorty.js
    testing logic function 2

--- src/components/RickandMorty.js
+++ src/components/RickandMorty.js
@@ -1,15 +1,17 @@
 import React, {useEffect, useState} from 'react'
 
 function RickandMorty({array}) {
+  
+  const display = () =>{
+    if(array[0] != null){
+      return array[0].name
+    }
+  }
 
   return (
     <div>
       {console.log(array[0])}
-      {
-        if(array[0] != null){
-          array[0].name
-        }
-      }
+      {display()}
     </div>
   )
 }
