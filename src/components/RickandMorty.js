b5435bdfe29307833530e42e00c2cc78cfc74b92
hafizn24 <41638447+hafizn24@users.noreply.github.com>
2022-09-07 12:43:42 +0800

    Update RickandMorty.js

--- src/components/RickandMorty.js
+++ src/components/RickandMorty.js
@@ -1,17 +1,24 @@
 import React, {useEffect, useState} from 'react'
 
 function RickandMorty({array}) {
+  const name = ''
+  const status = ''
+  const species = ''
   
   const display = () =>{
     if(array[array.length - 1] != null){
-      return(array[array.length - 1].name)
+      name = array[array.length - 1].name
+      status = array[array.length - 1].status
+      species = array[array.length - 1].species
     }
   }
 
   return (
     <div>
       {console.log(array[array.length - 1])}
-      {display()}
+      {name}
+      {status}
+      {species}
     </div>
   )
 }
