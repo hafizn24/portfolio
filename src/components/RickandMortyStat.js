1d808276b8a8e27287b3b08a9dcd0f100cf4c909
hafiz <hafizn24@gmail.com>
2022-09-11 16:49:12 +0800

    adjust the ui for rick and morty

--- src/components/RickandMortyStat.js
+++ src/components/RickandMortyStat.js
@@ -4,7 +4,13 @@ import React from 'react'
 function RickandMortyStat({list ,title}) {
     function countFunc(){
         let tempmap =  new Map()
-        for(let i in unique){
+        let size = 0
+        if(unique.length > 3){
+            size = 3
+        }else{
+            size = unique.length
+        }
+        for(let i = 0; i < size; i++){
             let index = 0
             for(let j in list){
                 if(list[j] == unique[i]){
@@ -18,12 +24,12 @@ function RickandMortyStat({list ,title}) {
 
     const unique = [...new Set(list)]
     const count = countFunc()
+
     const countDisplay = Array.from(
-        count.entries(), ([key, value]) => 
-        <>
+        count.entries(), ([key, value]) =>
+        <> 
             <Typography variant="body2">{key + ": "}<b>{value}</b></Typography>
         </>
-        
     )
 
     return (
@@ -32,12 +38,13 @@ function RickandMortyStat({list ,title}) {
             my:1,
             px:5,
             pt:2,
-            pb:5
+            pb:5,
+            minWidth: 300
         }}>
             <Grid
                 container
                 direction="row"
-                justifyContent="center"
+                justifyContent="left"
                 alignItems="center"
             >
                 <Typography variant="h4" color="secondary">
@@ -47,8 +54,8 @@ function RickandMortyStat({list ,title}) {
             <Grid
                 container
                 direction="column"
-                justifyContent="center"
-                alignItems="center"
+                justifyContent="left"
+                alignItems="left"
             >
                 <Box sx={{p:1}}>
                    {countDisplay}
